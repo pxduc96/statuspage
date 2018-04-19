8295dcf7675da13d1ffdd62cd208412dc91fd89d
pxduc96 <pxduc96@gmail.com>
2018-04-19 10:53:48 +0700

    sdff

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ except BaseException as e:
     raise e
 
 
-
 try:
-    # app.run(host="0.0.0.0", port=8080, debug="True")
+    app.run(host="0.0.0.0", port=8080, debug="True")
 
     # from models import *
 
